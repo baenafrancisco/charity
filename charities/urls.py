499dd922d328ce00edbb78bb2fcc752ce84fd8ca
Francisco Baena <soy@franbaena.com>
2015-02-25 23:41:00 +0000

    Implemented get_next_charities API Call

--- charities/urls.py
+++ charities/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^$', 'www.views.home', name='home'),
     url(r'^api/api_call_example/$', 'www.views.api_call_example', name='api_call_example'),
     url(r'^api/api_call_example/2/$', 'www.views.api_call_example2', name='api_call_example2'),
+    url(r'^api/getnextcharities/$', 'www.views.get_next_charities', name='get_next_charities'),
 
 )
--- www/views.py
+++ www/views.py
@@ -51,3 +51,54 @@ def api_call_example2(request):
 				'user_balances' : user_balances }
 	return HttpResponse(json.dumps(response), content_type="application/json")
 
+
+def get_next_charities(request):
+
+	'''
+	This function should return next n (GET) charities.
+	If n is not specified, default is 10
+	'''
+
+
+	# Get request parameters
+	n = 10
+
+	if 'n' in request.GET:
+		n = request.GET['n']
+
+	# Initialize response variable
+	charities = []
+
+	# We find the logged-in user_profile
+
+	try:
+		user_profile = UserProfile.objects.get(user=request.user)
+	except:
+		# Hackathon Error Handling: if there is any error
+		# we will get 1st user profile
+		user_profile = UserProfile.objects.get(user=1)
+
+	# We get a random set of the next n charities 
+	# that the user hasn't already seen
+
+	remaining_charities = Charity.objects.all().order_by('?').exclude(pk__in=user_profile.seen.all())[:n]
+
+	# And iterate through the list
+	# in order to add the records to the response
+	for charity in remaining_charities:
+		# For that we create a new object
+		new_record = {
+			'id':charity.pk,
+			'name':charity.name,
+			'description':charity.description,
+			'images':[]
+		}
+		# And iterate through the M2M relationship of the object
+		for image in charity.images.all():
+			new_record['images'].append(image.url)
+		# Before exit the bucle, we add the new record to the response
+		charities.append(new_record)
+
+	# We return an HTTP response with the information in JSON
+	return HttpResponse(json.dumps(charities), content_type="application/json")
+
