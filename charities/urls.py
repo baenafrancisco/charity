c843e07fae1e566c7fa62bbde6586303dea1255d
Raphael <Darryl@10-x-x-x.solent.ac.uk>
2015-02-25 21:43:51 +0000

    Updated home.html

--- charities/urls.py
+++ charities/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'www.views.home', name='home'),
 )
--- www/views.py
+++ www/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+
+	context = { 'name':'darryl'}
+
+	# one line comment
+
+	'''
+	Many many lines 
+	'''
+	return render(request,'home.html', context)
